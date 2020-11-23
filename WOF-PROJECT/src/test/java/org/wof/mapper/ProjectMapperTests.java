d2332268a7427eda4ad33305bfbbe3d76f969e11
hyunghunkim <khj923265@naver.com>
2020-11-23 09:25:18 +0900

    hyungjun branch first commit

--- WOF-PROJECT/src/test/java/org/wof/mapper/ProjectMapperTests.java
+++ WOF-PROJECT/src/test/java/org/wof/mapper/ProjectMapperTests.java
@@ -1,28 +1,28 @@
-package org.wof.mapper;
-
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.wof.persistence.DataSourceTests;
-
-import lombok.Setter;
-import lombok.extern.log4j.Log4j;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
-@Log4j
-public class ProjectMapperTests {
-
-	@Setter(onMethod_=@Autowired)
-	private ProjectMapper project;
-	
-	@Test
-	public void test() {
-		log.info(project);
-	}
-
-}
+//package org.wof.mapper;
+//
+//import static org.junit.Assert.*;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.wof.persistence.DataSourceTests;
+//
+//import lombok.Setter;
+//import lombok.extern.log4j.Log4j;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+//@Log4j
+//public class ProjectMapperTests {
+//
+//	@Setter(onMethod_=@Autowired)
+//	private ProjectMapper project;
+//
+//	@Test
+//	public void test() {
+//		log.info(project);
+//	}
+//
+//}
