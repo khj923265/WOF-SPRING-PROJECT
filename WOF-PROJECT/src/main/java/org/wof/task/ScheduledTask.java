fdac15a0971af09718703a4b504b1c3a9403a641
hyunghunkim <khj923265@naver.com>
2020-11-25 11:52:24 +0900

    xml 에러 수정

--- WOF-PROJECT/src/main/java/org/wof/task/ScheduledTask.java
+++ WOF-PROJECT/src/main/java/org/wof/task/ScheduledTask.java
@@ -1,5 +1,6 @@
 package org.wof.task;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -8,14 +9,14 @@ import org.wof.service.ProjectService2;
 
 import lombok.extern.log4j.Log4j;
 
-/*@Log4j
+@Log4j
 @Component
+@AllArgsConstructor
 public class ScheduledTask {
 	
-	@Autowired
+
 	private ProjectMapper2 projectMapper2;
 	
-	@Autowired
 	private ProjectService2 projectService2;
 
 	// 留ㅼ씪 �깉踰� 3�떆�뿉 �봽濡쒖젥�듃 �긽�깭 �뾽�뜲�씠�듃 �떆�궎�뒗 �뒪耳�以꾨윭
@@ -39,4 +40,4 @@ public class ScheduledTask {
 	
 	
 
-}*/
+}
--- WOF-PROJECT/src/test/java/org/wof/mapper/ProjectMapperTests.java
+++ WOF-PROJECT/src/test/java/org/wof/mapper/ProjectMapperTests.java
@@ -10,8 +10,10 @@ import lombok.Setter;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.wof.domain.FollowProjectVO;
 import org.wof.domain.ProjectVO;
 import org.wof.persistence.DataSourceTests;
@@ -21,6 +23,7 @@ import lombok.extern.log4j.Log4j;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
+@WebAppConfiguration
 public class ProjectMapperTests {
 
 	@Setter(onMethod_ = @Autowired)
@@ -29,7 +32,7 @@ public class ProjectMapperTests {
 	@Setter(onMethod_ = @Autowired)
 	private ProjectMapper pm1;
 
-	
+
 
 	/*@Test
 	public void testLocation() {
@@ -44,13 +47,13 @@ public class ProjectMapperTests {
 		vo.setRelated_project("project1");
 		log.info(projectMapper2.addFollowProject(vo));
 	}*/
-	
+
 /*	@Test
 	public void testdeleteFollowProjectMapper() {
 		//로그인된 파트너 회원의 팔로우 프로젝트를 지울 수 있다.
 		log.info(projectMapper2.deleteFollowProject("project1"));
 	}*/
-	
+
 /*	@Test
 	public void testpageFollowProjectList(){
 		Criteria cri = new Criteria();
@@ -59,7 +62,7 @@ public class ProjectMapperTests {
 		List<ProjectVO> list = projectMapper2.ProjectList(cri);
 		list.forEach(project -> log.info(project.getProj_id()));
 	}*
-	
+
 /*	@Test
 	public void testPageMapper(){
 		//검색과 페이징처리 한번에하기
@@ -68,12 +71,12 @@ public class ProjectMapperTests {
 		cri.setType("TC");
 		log.info(projectMapper2.ProjectList(cri));
 	}*/
-	
+
 /*	@Test
 	public void testFollowProjectList(){
 		log.info(projectMapper2.followProjectList());
 	}*/
-	
+
 /*	@Test
 	public void testProjectStatusUpdate(){
 		log.info(projectMapper2.projectStatusUpdate());
@@ -84,7 +87,7 @@ public class ProjectMapperTests {
 		pm1.getList().forEach(project -> log.info(project));
 	}
 */
-	
+
 	@Test
 	public void insertProject(){
 		ProjectVO p1 = new ProjectVO();
@@ -104,7 +107,7 @@ public class ProjectMapperTests {
 		p1.setProj_work_equip(1);
 		p1.setProj_walfare(0);
 		p1.setProj_detail("프로젝트 개요 :- quant 주식 투자 서비스 프로젝트 우선순위 :- 프로젝트 기간 준수가 가장 중요하며, 가능하다면 기간이 단축되어야 합니다."
-				+ "프로젝트의 목적 : " 
+				+ "프로젝트의 목적 : "
 				+ " - 베타 버전을 넘어서 결제등의 서비스가 되어 실제 서비스가 가능한 정도의 제품 개발"
 				+ "- 기획안이 나와 있으며, 예전에 웹으로 베타버전을 만들었던 적이 있음"
 				+ "- 최대한 빠른 기간안에 완료 되면 좋음"
@@ -124,23 +127,23 @@ public class ProjectMapperTests {
 				+ "- http://www.quantking.co.kr"
 				+ "- 기능 및 UI/UX 는 거의 동일, 추가 서비스 몇개는 추가될 수 있음"
 				+ "기타 참고 사항/유의 사항 :");
-		
-		
+
+
 		pm1.insertProject(p1);
 		log.info(p1);
 	}
-	
+
 	/*@Test
 	public void projectRead(){
 		ProjectVO p1 = pm1.readProject("project8");
 		log.info(p1);
 	}*/
-	
+
 	/*@Test
 	public void ProjectDelete(){
 		log.info("DELETE COUNT: "+pm1.deleteProject("project8"));
 	}*/
-	
+
 /*	@Test
 	public void testLocation() {
 		Random rm = new Random();
@@ -200,10 +203,11 @@ public class ProjectMapperTests {
 		log.info(projectMapper2.followProjectList());
 	}*/
 
-//	@Test
-//	public void testProjectStatusUpdate(){
-//		log.info(projectMapper2.followProjectList());
-//	}
+	@Test
+	@WithUserDetails
+	public void testProjectStatusUpdate(){
+		log.info(projectMapper2.projectList());
+	}
 
 
 
