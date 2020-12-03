27ef3927cc262b34074964b771d4b328d7e8b55c
choi <nojak@naver.com>
2020-12-03 10:02:09 +0900

    계약완료 메뉴 진행중

--- WOF-PROJECT/src/main/java/org/wof/domain/ContractVO.java
+++ WOF-PROJECT/src/main/java/org/wof/domain/ContractVO.java
@@ -1,5 +1,7 @@
 package org.wof.domain;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
@@ -10,5 +12,20 @@ public class ContractVO {
 	private String contract_write_target;
 	private String source_signature;
 	private String target_signature;
+	
+	private String contract_reg_date;
+	private String sourceName;
+	private String sourcePhone;
+	private String targetName;
+	private String targetPhone;
+	
+	private int business_register_no; //PK
+    private String proj_id; //FK 
+    private String client;
+    private String partner;
+    private int proj_estimate;
+    private String proj_title;
+    private Date proj_start_date;
+    private Date proj_end_date; 
 
 }
